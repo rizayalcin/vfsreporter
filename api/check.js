9b2cdf6a9fec8a21fc748dcf6755d2f1f1194aa9
rizayalcin <rzyalcin@gmail.com>
2024-10-09 10:59:10 +0300

    Create check.js

---
+++ api/check.js
@@ -0,0 +1,11 @@
+const main = require('../index');
+
+module.exports = async (req, res) => {
+    try {
+        await main();
+        res.status(200).send('Appointment check completed successfully');
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('An error occurred: ' + error.message);
+    }
+};
